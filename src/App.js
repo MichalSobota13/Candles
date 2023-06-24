6469222b729f828583abd028c0a5c10460cbd810
MichalSobota13 <michalsobota13@gmail.com>
2023-06-24 16:28:54 +0200

    done scrolltop component

--- src/App.js
+++ src/App.js
@@ -9,39 +9,43 @@ import {
   AboutUs,
   Login,
 } from "./pages";
+import ScrollTop from "./ScrollTop";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Layout />}>
+    <>
+      <ScrollTop />
+      <Routes>
         <Route
-          index
-          element={<HomePage />}
-        />
-        <Route
-          path="produkty"
-          element={<Products />}
-        />
-        <Route
-          path="produkty/:id"
-          element={<Product />}
-        />
-        <Route
-          path="o-nas"
-          element={<AboutUs />}
-        />
-        <Route
-          path="koszyk"
-          element={<Basket />}
-        />
-        <Route
-          path="login"
-          element={<Login />}
-        />
-      </Route>
-    </Routes>
+          path="/"
+          element={<Layout />}>
+          <Route
+            index
+            element={<HomePage />}
+          />
+          <Route
+            path="produkty"
+            element={<Products />}
+          />
+          <Route
+            path="produkty/:id"
+            element={<Product />}
+          />
+          <Route
+            path="o-nas"
+            element={<AboutUs />}
+          />
+          <Route
+            path="koszyk"
+            element={<Basket />}
+          />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
